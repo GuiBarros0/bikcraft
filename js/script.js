be5fa49d3acd6ddc8b8e9bd1165f1b076d2669fa
Gui Barros <guisanches.exe@gmail.com>
2024-03-14 16:37:53 -0300

    feat: adicionado passagem de parametro para seleção de items

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("teste");
+//ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//Ativar items do Orçamento
+const parametros = new URLSearchParams(window.location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
+}
+
+parametros.forEach(ativarProduto);
